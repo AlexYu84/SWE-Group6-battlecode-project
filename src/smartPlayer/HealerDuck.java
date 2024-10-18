d097e70bc95cbbddf1d3e50cf283ef5dced5db48
Devam Patel <devamp@pdx.edu>
2024-10-18 13:40:16 -0700

    setup project structure

---
+++ src/smartPlayer/HealerDuck.java
@@ -0,0 +1,33 @@
+package smartPlayer;
+
+import battlecode.common.Clock;
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public class HealerDuck {
+
+    public static void run(RobotController rc) throws GameActionException {
+        while (true) {
+
+            try {
+                if (rc.isSpawned()) {
+
+                    // logic for builder that is spawned
+                    System.out.println("Healer Running...");
+
+                } else {
+
+                    // error log here
+
+                }
+
+            }  catch (Exception e) {
+                System.out.println("Exception");
+                e.printStackTrace();
+
+            } finally {
+                Clock.yield();
+            }
+        }
+    }
+}
---
+++ src/smartPlayer/RobotPlayer.java
@@ -0,0 +1,69 @@
+package smartPlayer;
+
+import battlecode.common.*;
+import javafx.util.Builder;
+import smartPlayer.AttackDuck;
+import smartPlayer.BuilderDuck;
+import smartPlayer.HealerDuck;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public strictfp class RobotPlayer {
+
+    static int turnCount = 0;
+
+    static final Random rng = new Random(6147);
+    static final Direction[] directions = {
+            Direction.NORTH,
+            Direction.NORTHEAST,
+            Direction.EAST,
+            Direction.SOUTHEAST,
+            Direction.SOUTH,
+            Direction.SOUTHWEST,
+            Direction.WEST,
+            Direction.NORTHWEST,
+    };
+
+    public static void run(RobotController rc) throws GameActionException {
+        while (true) {
+            turnCount += 1;
+
+            try {
+                if (!rc.isSpawned()) {
+                    MapLocation[] spawnLocs = rc.getAllySpawnLocations();
+                    MapLocation randomLoc = spawnLocs[rng.nextInt(spawnLocs.length)];
+
+                    // Spawn and assign roles based on the current counts
+                    if (rc.canSpawn(randomLoc)) {
+                        rc.spawn(randomLoc);
+
+                        int randomDuckType = rng.nextInt(3);
+
+                        switch(randomDuckType) {
+                            case 0:
+                                AttackDuck.run(rc);
+                            case 1:
+                                HealerDuck.run(rc);
+                            case 2:
+                                BuilderDuck.run(rc);
+                        }
+
+                    }
+                }
+
+            } catch (GameActionException e) {
+                System.out.println("GameActionException");
+                e.printStackTrace();
+
+            } catch (Exception e) {
+                System.out.println("Exception");
+                e.printStackTrace();
+
+            } finally {
+                Clock.yield();
+            }
+        }
+    }
+}
