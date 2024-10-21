c986013b1500d2e4b8e9fcc5b0481fd322f95dcb
Alex  Yu <alexyu478@gmail.com>
2024-10-20 18:28:23 -0700

    attack duck robot
    implemented very simple attack robot

--- src/smartPlayer/AttackDuck.java
+++ src/smartPlayer/AttackDuck.java
@@ -1,30 +1,37 @@
 package smartPlayer;
 
-import battlecode.common.Clock;
-import battlecode.common.GameActionException;
-import battlecode.common.RobotController;
+import battlecode.common.*;
 
 public class AttackDuck {
-
     public static void run(RobotController rc) throws GameActionException {
         while (true) {
-
             try {
                 if (rc.isSpawned()) {
-
-                    // logic for builder that is spawned
+                    // Attack logic for the attacker duck
                     System.out.println("Attacker Running...");
 
+                    // Sense nearby enemy robots
+                    RobotInfo[] enemies = rc.senseNearbyRobots(-1, rc.getTeam().opponent());
+
+                    if (enemies.length > 0) {
+                        // Move towards the nearest enemy
+                        MapLocation enemyLocation = enemies[0].location;
+                        if (rc.canMove(rc.getLocation().directionTo(enemyLocation))) {
+                            rc.move(rc.getLocation().directionTo(enemyLocation));
+                        }
+
+                        // Attack the enemy if within range
+                        if (rc.canAttack(enemyLocation)) {
+                            rc.attack(enemyLocation);
+                        }
+                    }
                 } else {
-
-                    // error log here
-
+                    // Error log here
+                    System.err.println("Attacker not spawned properly.");
                 }
-
-            }  catch (Exception e) {
-                System.out.println("Exception");
+            } catch (Exception e) {
+                System.err.println("Exception: " + e.getMessage());
                 e.printStackTrace();
-
             } finally {
                 Clock.yield();
             }
