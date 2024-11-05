d0e65e2ad5f10e236c0325a55071c31a1e5878af
Adan Hernandez <adanh74379@gmail.com>
2024-11-04 21:54:36 -0800

    passing test cases and minor changes for PathFind

---
+++ test/smartPlayer/HealerDuckTest.java
@@ -0,0 +1,39 @@
+//package smartPlayer;
+//
+//import battlecode.common.GameActionException;
+//import battlecode.common.MapLocation;
+//import battlecode.common.RobotController;
+//
+//import battlecode.world.GameWorld;
+//import battlecode.world.InternalRobot;
+//import battlecode.world.RobotControllerImpl;
+//import org.junit.Test;
+//import static org.junit.Assert.*;
+//import static org.mockito.Mockito.*;
+//
+//public class HealerDuckTest {
+//
+//    @Test
+//    public void testHealerDuck() {
+//
+////        GameWorld gw = mock(GameWorld.class);
+////        InternalRobot internalRobot = mock(InternalRobot.class);
+////        RobotController robotController = new RobotControllerImpl(gw,internalRobot);
+//        RobotController rc = mock(RobotController.class);
+//        RobotPlayer player = new RobotPlayer();
+//
+////        try {
+////            rc.spawn(new MapLocation(0, 0));
+////        } catch (GameActionException e) {
+////            throw new RuntimeException(e);
+////        }
+////        HealerDuck healerDuck = new HealerDuck();
+//        try {
+//            player.run(rc);
+//        } catch (GameActionException e) {
+//            throw new RuntimeException(e);
+//        }
+////        verify(robotController, times(1)).isSpawned();
+//        assertTrue(rc.isSpawned());
+//    }
+//}
